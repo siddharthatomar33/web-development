4b38eb428de42725fb565db46565a0572545cc3c
siddharthatomar33 <siddharthatomar33@gmail.com>
2024-12-20 22:37:13 +0530

    web dev

---
+++ login.h
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link src="login.html">
+    <title>Document</title>
+</head>
+<body>
+        <h1>Login</h1>
+        <label>username:</label>
+        <input type="text"></body>
+        <br>
+        <label>password</label>
+        <input type="password"></input>
+        <br>
+        <button type="submit">submit</button>
+</body>
+</html>
