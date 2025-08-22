e897a14d36a938e18961dfb00f8a8c8f29acdeba
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-08-22 20:12:11 +0530

    js practise

--- classwork/script.js
+++ classwork/script.js
@@ -5,8 +5,14 @@ const increment=()=>{
     let value= parseInt(countvalue.innertext);
     //update the value
     value=value+1;
+    //set the value back to UI
     countvalue.innertext=value;
 };
 const decrement=()=>{
+    let value= parseInt(countvalue.innertext);
+    //update the value
+    value=value-1;
+    //set the value back to UI
+    countvalue.innertext=value;
 
 }
