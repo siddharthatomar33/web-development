a1623b5f43e4c3b01bdcca66bc404dae2f1c0994
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-08-22 20:09:52 +0530

    js practise

---
+++ classwork/script.js
@@ -0,0 +1,11 @@
+const countvalue=document.querySelector('#counter');
+
+const increment=()=>{
+    //get the
+    let value= parseInt(countvalue.innertext);
+    value=value+1;
+    countvalue.innertext=value;
+};
+const decrement=()=>{
+
+}
