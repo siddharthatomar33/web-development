06b028a8bf626cdf5224771b584eb7e52b59e31c
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-08-22 20:23:08 +0530

    js practise

--- classwork/script.js
+++ classwork/script.js
@@ -2,7 +2,7 @@ const countvalue=document.querySelector('#counter');
 
 const increment= () => {
     //get the value from the UI
-    let value= parseInt(countvalue.innerText);
+    let value= parseInt(countvalue.innerText);//paraseInt converts string to integer
     //update the value
     value=value+1;
     //set the value back to UI
