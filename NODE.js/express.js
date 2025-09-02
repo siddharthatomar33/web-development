8dec4ff4e7e631043d0189b3dc65db570e8a51b4
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-09-02 09:27:51 +0530

    js practise

--- NODE.js/express.js
+++ NODE.js/express.js
@@ -1,6 +1,17 @@
 const express=require('express');
 const app=express();
 
+app.get('/',(req,res)=>{
+    res.send("This is home page");
+});
+app.get('/about',(req,res)=>{
+    res.send("This is about page");
+});
+app.get('/contact',(req,res)=>{
+    res.send("This is contact page");
+}); 
+
+
 app.listen(8000,()=>{
     console.log("server is loading");
 });
